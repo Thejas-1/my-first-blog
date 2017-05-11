b6373e7eee749fa49a0ae39a8aea8061b54c38c3
Thejas-1 <naiktheju@gmail.com>
2017-05-11 20:32:06 +0530

    Changes made to the template in order to match the different user data based on a particular criteria

--- djangogirls/mysite/mysite/settings.py
+++ djangogirls/mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gag1for7imm6%mm7$2820-8h!f)k$z6bz4o*e*f*rm2-#_9b1v'
+SECRET_KEY = '&_gn6l_h+dghh7w74j&6j1=@xsym66c@4*11fax6@3=dsm(!t_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -120,5 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
